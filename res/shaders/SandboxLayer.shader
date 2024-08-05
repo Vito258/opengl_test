e0497930b934ec0da890f67f5720a899ffecf4c5
Vito258 <3090095224@qq.com>
2024-08-05 15:26:16 +0800

    29.批量渲染-关于颜色

--- res/shaders/SandboxLayer.shader
+++ res/shaders/SandboxLayer.shader
@@ -2,9 +2,12 @@
  #version 330 core
 
  layout(location = 0) in vec4 position;
+ layout(location = 1) in vec4 a_Color;
 
+ out vec4 v_Color;
  void main()
  {
+     v_Color = a_Color;
      gl_Position =  position;
  };
 
@@ -12,9 +15,8 @@
  #version 330 core
 
  layout(location = 0) out vec4 color;
-
- uniform vec4 u_Color;
+ in vec4 v_Color;
  void main()
  {
-    color = u_Color;
+    color = v_Color;
  };
--- src/SandboxLayer.cpp
+++ src/SandboxLayer.cpp
@@ -22,70 +22,71 @@
 
 //渲染两个正方形
 int main() {
-        GLFWwindow *window;
+    GLFWwindow *window;
 
-        /* Initialize the library */
-        if (!glfwInit())
-            return -1;
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
 
 
-        // 设置期望的OPENGL 版本
-        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    // 设置期望的OPENGL 版本
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 
-        //设置核心配置文件
-        //这样设置会报错，提示1282 既没有顶点数组对象，需要自己显式设置
-        glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    //设置核心配置文件
+    //这样设置会报错，提示1282 既没有顶点数组对象，需要自己显式设置
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-        window = glfwCreateWindow(1080, 970, "Hello World", NULL, NULL);
-        if (!window) {
-            glfwTerminate();
-            return -1;
-        }
+    window = glfwCreateWindow(1080, 970, "Hello World", NULL, NULL);
+    if (!window) {
+        glfwTerminate();
+        return -1;
+    }
 
-        /* Make the window's context current */
-        //用于得到一个渲染openGL的上下文
-        glfwMakeContextCurrent(window);
+    /* Make the window's context current */
+    //用于得到一个渲染openGL的上下文
+    glfwMakeContextCurrent(window);
 
-        //同步v-sync 或者时主频刷新率
-        glfwSwapInterval(1);
+    //同步v-sync 或者时主频刷新率
+    glfwSwapInterval(1);
 
-        //改为使用GLEW
-        GLenum err = glewInit();
-        if (GLEW_OK != err) {
-            /* Problem: glewInit failed, something is seriously wrong. */
-            fprintf(stderr, "Error: %s\n", glewGetErrorString(err));
-        }
-        fprintf(stdout, "Status: Using GLEW %s\n", glewGetString(GLEW_VERSION));
+    //改为使用GLEW
+    GLenum err = glewInit();
+    if (GLEW_OK != err) {
+        /* Problem: glewInit failed, something is seriously wrong. */
+        fprintf(stderr, "Error: %s\n", glewGetErrorString(err));
+    }
+    fprintf(stdout, "Status: Using GLEW %s\n", glewGetString(GLEW_VERSION));
 
     {
         float positions[] = {
-                -0.2f, -0.2f,  //0
-                0.2f, -0.2f,   //1
-                0.2f, 0.2f,    //2
-                -0.2f, 0.2f,   //3
+                -0.2f, -0.2f, 0.18f, 0.6f, 0.96f, 1.0f,  //0
+                0.2f, -0.2f, 0.18f, 0.6f, 0.96f, 1.0f, //1
+                0.2f, 0.2f, 0.18f, 0.6f, 0.96f, 1.0f, //2
+                -0.2f, 0.2f, 0.18f, 0.6f, 0.96f, 1.0f, //3
 
-                0.4f, -0.2f,   //4
-                0.8f,-0.2f,  //5
-                0.8f,0.2f,  //6
-                0.4f,0.2f    //7
+                0.4f, -0.2f, 1.0f, 0.93f, 0.24f, 1.0f,//4
+                0.8f, -0.2f, 1.0f, 0.93f, 0.24f, 1.0f,//5
+                0.8f, 0.2f, 1.0f, 0.93f, 0.24f, 1.0f, //6
+                0.4f, 0.2f, 1.0f, 0.93f, 0.24f, 1.0f,//7
         };
 
         unsigned int indices[] = {
                 0, 1, 2,
                 2, 3, 0,
-                4,5,6,
-                6,7,4
+                4, 5, 6,
+                6, 7, 4
         };
 
         //显式创建一个顶点数组
         VertexArray va;
 
         //创建顶点缓冲区 会自动绑定
-        VertexBuffer vb(positions, 8 * 2 * sizeof(float));
+        VertexBuffer vb(positions, 8 * 6 * sizeof(float));
 
         VertexBufferLayout layout;
         GlCall(layout.push<float>(2));
+        GlCall(layout.push<float>(4));
         GlCall(va.AddBuffer(vb, layout));
 
         //索引缓冲区的设置
@@ -94,7 +95,6 @@ int main() {
         //读取着色器源码
         Shader shader("../res/shaders/SandboxLayer.shader");
         shader.Bind();
-        shader.SetUniform4f("u_Color", 1.0f, 1.0f, 1.0f, 1.0f);
         // 解绑
         va.UnBind();
         shader.UnBind();
@@ -111,7 +111,7 @@ int main() {
             shader.Bind();
 
             // 渲染器绘制
-            renderer.Draw(va,ib,shader);
+            renderer.Draw(va, ib, shader);
 
             /* Swap front and back buffers */
             GlCall(glfwSwapBuffers(window));
